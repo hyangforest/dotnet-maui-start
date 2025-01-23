8e74f34b6f0ed828c41f2e9ce51a473abd701560
Hyangrim Lee <hyangrim.lee@kims.co.kr>
2025-01-23 16:47:59 +0900

    설정 v.1.1

--- App.xaml.cs
+++ App.xaml.cs
@@ -8,5 +8,22 @@
 
             MainPage = new AppShell();
         }
+
+        // 앱이 처음 실행될 때 플랫폼별 수명 주기 이벤트를 재정의할 수 있음.
+        //protected override void OnStart()
+        //{
+        //    base.OnStart();
+        //}
+
+        //protected override void OnResume()
+        //{
+        //    base.OnResume();
+        //}
+
+        //protected override void OnSleep()
+        //{
+        //    base.OnSleep();
+        //}
+        // 앱이 처음 실행될 때 플랫폼별 수명 주기 이벤트를 재정의할 수 있음.
     }
 }
